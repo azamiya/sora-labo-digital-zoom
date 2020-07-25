ad5c9e9603ddf405095709dc658864facd6281aa
azamiya <t.adaniya@adawarp.com>
2020-07-25 19:58:40 +0900

    feat(receiver): implement sora connection

---
+++ consts/endpoints.js
@@ -0,0 +1,1 @@
+export const SORA_LABO_WS_URL = "wss://sora-labo.shiguredo.jp/signaling"
--- pages/receiver.js
+++ pages/receiver.js
@@ -1,13 +1,77 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
+import {SORA_LABO_WS_URL} from "../consts/endpoints";
+import Sora from '../assets/javascript/sora'
 
 const ReceiverPage = () => {
+
+  const [githubUserName, inputGithubUserName] = useState('')
+  const [signalingKey, inputSignalingKey] = useState('')
+  const [channelName, inputChannelName] = useState('digitalZoomSample')
+
+  const onConnectButtonClick = () => {
+    const metadata = {
+      signaling_key: signalingKey,
+      turn_tcp_only: false,
+      turn_tls_only: false
+    }
+
+    const soraConnection = Sora.connection(SORA_LABO_WS_URL);
+    console.log(soraConnection)
+    const channelId = `${githubUserName}@${channelName}`
+    const recvonly = soraConnection.recvonly(channelId, metadata);
+    recvonly.connect()
+      .catch(e => {
+        console.error(e);
+      });
+  }
+
+  useEffect(() => {
+    const _githubUserName = localStorage.getItem('_githubUserName')
+    if(_githubUserName) {
+      inputGithubUserName(_githubUserName)
+    }
+    const _signalingKey = localStorage.getItem('_signalingKey')
+    if(_signalingKey) {
+      inputSignalingKey(_signalingKey)
+    }
+  }, [])
+
   return (
     <div>
       <Link href="/">
         <a>&#8826;- HOME</a>
       </Link>
       <h1>Receiver</h1>
+      <div>
+        <p>Github user name</p>
+        <input value={githubUserName} onChange={e => {
+          localStorage.setItem('_githubUserName', e.target.value)
+          inputGithubUserName(e.target.value)
+        }} />
+        <p>Signaling key</p>
+        <input value={signalingKey} onChange={e => {
+          localStorage.setItem('_signalingKey', e.target.value)
+          inputSignalingKey(e.target.value)
+        }} />
+        <p>Channel name</p>
+        <input value={channelName} onChange={e => {
+          inputChannelName(e.target.value)
+        }} />
+      </div>
+      <div>
+        <button
+          className='connect'
+          onClick={onConnectButtonClick}>CONNECT</button>
+      </div>
+      <style jsx>{`
+        p {
+          margin: 1rem 0 0;
+        }
+        button.connect {
+          margin: 1rem 0;
+        }
+      `}</style>
     </div>
   )
 }
